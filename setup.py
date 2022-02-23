1c0c7fc3235de8a928405af22e54f31d7eda55bb
Richard Hughes <richard@hughsie.com>
2022-02-23 09:28:30 +0000

    Release 0.2.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ See also: https://msdn.microsoft.com/en-us/library/bb417343.aspx
 
 setup(
     name="cabarchive",
-    version="0.2.3",
+    version="0.2.4",
     license="LGPL-2.1-or-later",
     description="A pure-python library for creating and extracting cab files",
     long_description=long_desc,
@@ -29,7 +29,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Topic :: Utilities",
         "Topic :: System :: Archiving",
     ],
